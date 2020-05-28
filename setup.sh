6f69add39fababde2150e58a58a03d8870264045
Dario Trossero <dario.trossero@inallmedia.com>
2020-05-28 14:40:32 -0300

    Add missing file and fixes

--- setup.sh
+++ setup.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+echo "Installing packages...."
 sudo apt-get update; #update repository cache
 sudo apt-get install -y mc htop python3-pip deluge git python-pip build-essential libssl-dev libffi-dev python-dev python-setuptools python-pip python-dev git libssl-dev libxslt1-dev libxslt1.1 libxml2-dev libxml2 libssl-dev libffi-dev build-essential deluged deluge-web deluge-console python-mako unzip cron minidlna samba samba-common-bin rbp-userland-dev-osmc libvncserver-dev libconfig++-dev python-wheel python-cffi python-cryptography
 
 
+echo "Installing SickRage...."
 #SickChill
 sudo git clone https://github.com/SickChill/SickChill.git /opt/sickrage
 sudo chown -R osmc:osmc /opt/sickrage
@@ -10,14 +12,12 @@ sudo cp sickrage.service /lib/systemd/system
 sudo systemctl enable sickrage.service
 
 
+echo "Installing Deluge...."
 #Deluge
 #https://pimylifeup.com/raspberry-pi-deluge/
 deluged
 sudo pkill -i deluged
-echo "-- Deluge installation --"
-read -p 'Username: ' uservar
-read -sp 'Password: ' passvar
-echo "$uservar:$passvar:10" >> ~/.config/deluge/auth
+echo "Access to <ip>:9112 with password 'deluge'"
 deluge-console "config -s allow_remote True"
 deluged
 deluge-web -f
@@ -29,9 +29,18 @@ sudo systemctl enable deluged.service
 #VNC SERVER
 #https://discourse.osmc.tv/t/howto-install-a-vnc-server-on-the-raspberry-pi/1517
 
-
+echo "Installing Minidlna...."
 #minidlna
 sudo cp minidlna.conf /etc
 sudo cp minidlna-systemd.service /lib/systemd/system
 sudo systemctl enable minidlna-systemd.service
 
+
+#piwheels
+sudo mv pip.conf /etc
+
+echo "Other stuff"
+mkdir bin
+git clone https://gitlab.com/dariotrossero/coopeofertas.git
+pip3 install -r ~/bin/coopeofertas/back/requirements.txt
+pip3 install -r ~/bin/coopeofertas/web/requirements.txt
